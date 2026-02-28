f26625f0668bd7b3e2c2c5a36e08bcc5316db32b
Hemanth2890 <hemsum289@gmail.com>
2026-02-28 17:30:11 +0530

    Create llm_helper.py

---
+++ utils/llm_helper.py
@@ -0,0 +1,26 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import torch
+
+
+class LLMHelper:
+
+    def __init__(self):
+        self.device = "cpu"
+
+        self.tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
+        self.model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base")
+        self.model.to(self.device)
+
+    def generate(self, prompt, max_new_tokens=200):
+
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+
+        outputs = self.model.generate(
+            **inputs,
+            max_new_tokens=max_new_tokens,
+            temperature=0.7,
+            top_p=0.9,
+            do_sample=True
+        )
+
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
